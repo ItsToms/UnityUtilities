1360acb75c238fc5ec775ebc2c8f07c863eb29cf
toms <toms.a.jensen@gmail.com>
2019-02-11 23:07:37 -0800

    Stubbed out state machine.

---
+++ StateMachine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Utils
+{
+    public class StateMachine<TState> where TState : Enum
+    {
+        public StateContainter this[TState state] => throw new NotImplementedException();
+
+        public void Send<TMessage>(TMessage message) => throw new NotImplementedException();
+
+        public class StateContainter
+        {
+            public StateContainter Map<TMessage>(Func<TMessage, TState> mapping) => throw new NotImplementedException();
+        }
+    }
+}
